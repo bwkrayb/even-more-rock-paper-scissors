8fd4beda827c4566df033215fc904baa28a75d43
Brandon Kraybill <bwkrayb@gmail.com>
2023-03-22 22:45:21 -0500

    Follow instructions for round size
    The round sizes were supposed to be best out of 5
    But I didn't do that
    I also didn't have it prompt for typed input
    Added prompt() for typed input instead of buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-// let compPickNum = Math.floor(Math.random()*3)+1;
 let rockBtn = document.querySelector(".rock");
 let papeBtn = document.querySelector(".paper");
 let scisBtn = document.querySelector(".scissors");
@@ -28,50 +27,83 @@ compPickLogic()
 console.log(compPick)
 
 
-rockBtn.addEventListener("click", () => {
-	let userPick = rockBtn.getAttribute('id')
-	console.log(userPick)
-	gameLogic(userPick,compPick)
-});
+// rockBtn.addEventListener("click", () => {
+// 	let userPick = rockBtn.getAttribute('id')
+// 	console.log(userPick)
+// 	gameLogic(userPick,compPick)
+// });
 
-papeBtn.addEventListener("click", () => {
-	let userPick = papeBtn.getAttribute('id')
-	console.log(userPick)
-	gameLogic(userPick,compPick)
-});
+// papeBtn.addEventListener("click", () => {
+// 	let userPick = papeBtn.getAttribute('id')
+// 	console.log(userPick)
+// 	gameLogic(userPick,compPick)
+// });
 
-scisBtn.addEventListener("click", () => {
-	let userPick = scisBtn.getAttribute('id')
-	console.log(userPick)
-	gameLogic(userPick,compPick)
-});
-
-
+// scisBtn.addEventListener("click", () => {
+// 	let userPick = scisBtn.getAttribute('id')
+// 	console.log(userPick)
+// 	gameLogic(userPick,compPick)
+// });
 
 
-function gameLogic(u,c) {
+function playRound(u,c,i) {
 	if ((u === "scissors" && c === "paper") || (u === "rock" && c === "scissors") || (u === "paper" && c === "rock")) {
-		console.log("You win the round! " + u + " beats " + c +" !")
+		console.log("You win! " + u + " beats " + c +"!")
 		userScore = userScore + 1
 	} 
 	else if ((c === "scissors" && u === "paper") || (c === "rock" && u === "scissors") || (c === "paper" && u === "rock"))  {
-		console.log("You lose the round! " + c + " beats " + u +" !")
+		console.log("Computer wins! " + c + " beats " + u +" !")
 		computerScore = computerScore + 1
 	} else {
 		console.log("tie?")
 		console.log("computer chose " + c)
+		return false;
+	}
+}
+
+
+// function gameLogic() {
+// 	playRound(userPick,compPick)
+// 	compPickLogic()
+// 	if (userScore === 5) {
+// 		console.log("You won 5 rounds!")
+// 		reset()
+// 		return;
+// 	} else if (computerScore === 5) {
+// 		console.log("The computer won 5 rounds!")
+// 		reset()
+// 		return;
+// 	}
+// }
+
+
+let playBtn = document.querySelector(".play");
+
+playBtn.addEventListener("click", () => {
+	gameLogic()
+});
+
+function gameLogic() {
+	for (let i = 1; i <= 5; i++) {
+		console.log("Round: " + i)
+		compPickLogic()
+		userPick = prompt().toLowerCase()
+		if (playRound(userPick,compPick) ==false) {
+			i--
+			console.log("Replay round: " + i)
+		}
+
 	}
 	compPickLogic()
-	if (userScore === 5) {
-		console.log("You won 5 rounds!")
+	if (userScore > computerScore) {
+		console.log("You won! The score was " + userScore + " to " + computerScore + ".")
 		reset()
 		return;
-	} else if (computerScore === 5) {
-		console.log("The computer won 5 rounds!")
+	} else {
+		console.log("The computer beat you " + computerScore + " to " + userScore + ".")
 		reset()
 		return;
 	}
 }
-
 
 
