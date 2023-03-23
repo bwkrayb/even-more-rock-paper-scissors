4bba2db92324e74da79f17d34360301c5794fcb6
Brandon Kraybill <bwkrayb@gmail.com>
2023-03-22 22:57:19 -0500

    Add a README.md to tell about the project

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome to EVEN MORE ROCK PAPER SCISSORS!
+
+This is my Rock Paper Scissors JavaScript project. I have actually gotten this far in the course before, so this is my third version, but they were all several years apart. It's pretty interesting to see the choices I made back then vs the choices that I made now.
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 let rockBtn = document.querySelector(".rock");
 let papeBtn = document.querySelector(".paper");
 let scisBtn = document.querySelector(".scissors");
-let userScore = 0;
-let computerScore = 0;
+// let userScore = 0;
+// let computerScore = 0;
 
 // console.log(compPickNum);
 
@@ -24,7 +24,7 @@ function reset() {
 
 compPickLogic()
 
-console.log(compPick)
+// console.log(compPick)
 
 
 // rockBtn.addEventListener("click", () => {
@@ -48,15 +48,14 @@ console.log(compPick)
 
 function playRound(u,c,i) {
 	if ((u === "scissors" && c === "paper") || (u === "rock" && c === "scissors") || (u === "paper" && c === "rock")) {
-		console.log("You win! " + u + " beats " + c +"!")
+		console.log("You win! " + u.substr(0,1).toUpperCase() + u.slice(1) + " beats " + c +"!")
 		userScore = userScore + 1
 	} 
 	else if ((c === "scissors" && u === "paper") || (c === "rock" && u === "scissors") || (c === "paper" && u === "rock"))  {
-		console.log("Computer wins! " + c + " beats " + u +" !")
+		console.log("Computer wins! " + c.substr(0,1).toUpperCase() + c.slice(1) + " beats " + u +"!")
 		computerScore = computerScore + 1
 	} else {
-		console.log("tie?")
-		console.log("computer chose " + c)
+		console.log("Tie! The Computer also chose " + c + "!")
 		return false;
 	}
 }
@@ -84,24 +83,26 @@ playBtn.addEventListener("click", () => {
 });
 
 function gameLogic() {
+	userScore = 0;
+	computerScore = 0;
 	for (let i = 1; i <= 5; i++) {
 		console.log("Round: " + i)
 		compPickLogic()
 		userPick = prompt().toLowerCase()
 		if (playRound(userPick,compPick) ==false) {
-			i--
 			console.log("Replay round: " + i)
+			i--
 		}
 
 	}
 	compPickLogic()
 	if (userScore > computerScore) {
-		console.log("You won! The score was " + userScore + " to " + computerScore + ".")
-		reset()
+		console.log("VICTORY! The score was " + userScore + " to " + computerScore + ".")
+		// reset()
 		return;
 	} else {
-		console.log("The computer beat you " + computerScore + " to " + userScore + ".")
-		reset()
+		console.log("GAME OVER! The score was " + computerScore + " to " + userScore + ".")
+		// reset()
 		return;
 	}
 }
